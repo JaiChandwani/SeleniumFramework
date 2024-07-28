73f7096d2317f164d0e9a0cde90665ae2257826f
JaiChandwani <chandwanibrothers3@gmail.com>
2024-07-28 15:04:22 +0530

    Adding extent reports.

---
+++ web/src/test/java/base/ExtentReportListener.java
@@ -0,0 +1,50 @@
+package base;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import java.util.Objects;
+
+
+public class ExtentReportListener implements ITestListener {
+
+    private static ExtentReports extentReports;
+    private static ThreadLocal<ExtentTest> extentTest = new ThreadLocal<>();
+
+    @Override
+    public void onStart(ITestContext context) {
+        ExtentSparkReporter htmlReporter = new ExtentSparkReporter("src/test/resources/testReport/extent-report.html");
+        extentReports = new ExtentReports();
+        extentReports.attachReporter(htmlReporter);
+    }
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        ExtentTest test = extentReports.createTest(result.getMethod().getMethodName());
+        extentTest.set(test);
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        extentTest.get().pass("Test passed");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        extentTest.get().fail(result.getThrowable());
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        extentTest.get().skip("Test skipped");
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        extentReports.flush();
+    }
+}
--- web/src/test/java/testCase/LoginTest.java
+++ web/src/test/java/testCase/LoginTest.java
@@ -2,23 +2,16 @@ package testCase;
 import base.baseTest;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
-import org.testng.Assert;
 
 public class LoginTest extends baseTest {
     @Test
     public static void login() throws InterruptedException {
+        driver.findElement(By.xpath(loc.getProperty("UserName"))).sendKeys(data.getProperty("UserName"));
+        Thread.sleep(2000);
+        driver.findElement(By.xpath(loc.getProperty("password"))).sendKeys(data.getProperty("password"));
+        Thread.sleep(2000);
+        driver.findElement(By.xpath(loc.getProperty("Login"))).click();
 
-driver.findElement(By.xpath(loc.getProperty("Login"))).click();
-Thread.sleep(2000);
-driver.findElement(By.xpath(loc.getProperty("UserName"))).sendKeys(data.getProperty("UserName"));
-Thread.sleep(2000);
-driver.findElement(By.xpath(loc.getProperty("NextBtn"))).click();
-Thread.sleep(2000);
-driver.findElement(By.xpath(loc.getProperty("password"))).sendKeys(data.getProperty("password"));
-Thread.sleep(2000);
-driver.findElement(By.xpath(loc.getProperty("Signin"))).click();
-Thread.sleep(5000);
-Assert.assertTrue(driver.findElement(By.xpath(loc.getProperty("Logo"))).isDisplayed());
     }
 }
 
